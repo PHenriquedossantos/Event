ebc1f3d14408d7028d6d8397861f447c8cec9ab2
unknown <henrique.ph935@gmail.com>
2023-04-12 16:55:06 -0300

    Corrige erro de digitacao no cadastro.html e e cria requisicao do tipo get nas views

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
+    
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        print(username, email, senha, confirmar_senha)
+        return HttpResponse('Teste')
