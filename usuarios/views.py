59b14262753264d08a7a904dacc5b04b53b6d579
unknown <henrique.ph935@gmail.com>
2023-04-12 20:39:55 -0300

    Cria view de login

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -33,3 +34,15 @@ def cadastro(request):
 def login(request):
     if request.method == "GET":
         return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=username, password=senha)
+
+        if not user:
+            messages.add_message(request, constants.SUCCESS, 'Usuário ou senha inválidos!')
+            return redirect(reverse('login'))
+        
+        auth.login(request, user)
+        return redirect('/evento/novo_evento/')
