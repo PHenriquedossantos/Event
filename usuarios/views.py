f5fae5fab7ca970234eb1c4cfe8c430e1e617587
unknown <henrique.ph935@gmail.com>
2023-04-12 19:28:26 -0300

    Finaliza o metodo POST de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
-    
     elif request.method == "POST":
         username = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
-        print(username, email, senha, confirmar_senha)
-        return HttpResponse('Teste')
+
+        if not (senha == confirmar_senha):    
+            return redirect(reverse('cadastro'))
+        
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            return redirect(reverse('cadastro'))   
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        return redirect(reverse('login'))
